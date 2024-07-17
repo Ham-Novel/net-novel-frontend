b8b6c6b7b64303380ee55947e6d69f425b13a744
ned-razzz <smg9972@gmail.com>
2024-07-17 13:00:06 +0900

    vue router feature add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/test', component: HelloWorld.vue }
+    ]
+})
+
+const app = createApp(App)
+
+app.mount('#app')
