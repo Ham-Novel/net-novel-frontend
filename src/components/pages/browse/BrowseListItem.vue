092fdd0243fc2db21918c7ed412139d1454e4cc7
ned-razzz <smg9972@gmail.com>
2024-09-15 13:32:46 +0900

    feat: Browse 소설 아이템 상세 정보 구현

--- src/components/pages/browse/BrowseListItem.vue
+++ src/components/pages/browse/BrowseListItem.vue
@@ -8,7 +8,22 @@
         :link="{ name: 'novel', params: { id: props.novel.id } }"
     >
         <div class="detailed">
-            <div class="tag">
+            <p class="author">
+                <PencilLine size="17" stroke-width="2.3" />
+                {{ props.novel.authorName }}
+            </p>
+            <p class="stats">
+                <span> <Eye size="15" /> {{ props.novel.totalView }} </span>
+                <span>
+                    <Heart size="15" />
+                    {{ props.novel.totalFavorites }}
+                </span>
+                <span>
+                    <Clock8 size="15" />
+                    {{ formatUtil.formatRealTime(props.novel.latestUpdateAt) }}
+                </span>
+            </p>
+            <div class="tag" v-if="props.novel.tags.length !== 0">
                 <span v-for="tag in props.novel.tags" :key="tag.id" @click="browseTag(tag.id)"
                     >#{{ tag.name }}</span
                 >
@@ -22,7 +37,9 @@
 
 <script setup>
 import ListItem from "@/components/reusable/novel/ListItem.vue";
+import { Eye, Heart, Clock8, PencilLine } from "lucide-vue-next";
 import { useRouter } from "vue-router";
+import { formatUtil } from "@/hooks/format";
 
 const props = defineProps({
     novel: {
@@ -42,9 +59,33 @@ const browseTag = (tagId) => {
     height: 180px
 
 .detailed
+    margin-top: 8px
+    display: flex
+    flex-flow: column wrap
+    gap: 6px
+
+    .author
+        font-size: 13px
+        font-weight: 700
+        color: var(--bg-dark)
+        display: flex
+        align-items: center
+        gap: 2px
+
+
+    .stats
+        display: flex
+        flex-flow: row wrap
+        font-size: 12px
+        gap: 10px
+
+        span
+            flex-shrink: 0
+            display: flex
+            align-items: center
+            gap: 1px
 
     .tag
-        margin-top: 15px
         width: 500px
         height: 20px
 
@@ -52,6 +93,7 @@ const browseTag = (tagId) => {
 
         display: flex
         flex-flow: row wrap
+        align-items: center
         gap: 8px
 
 
@@ -66,13 +108,13 @@ const browseTag = (tagId) => {
                 text-decoration: underline
 
     .desc
-        margin-top: 10px
         max-height: 80px
-        font-size: 15px
+        font-size: 14px
         overflow: hidden
         overflow-wrap: break-word
         text-overflow: ellipsis
         display: -webkit-box
         -webkit-line-clamp: 3 /* 표시할 줄 수 */
         -webkit-box-orient: vertical
+        color: gray
 </style>
