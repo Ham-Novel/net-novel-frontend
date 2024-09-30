fafbc634fc9b459149c2fac4ee05a677c9e98c88
ned-razzz <smg9972@gmail.com>
2024-09-30 18:00:37 +0900

    feat: 작품 연재 상태 요소 추가 및 색상 반영

--- src/components/pages/library/FavoriteCardItem.vue
+++ src/components/pages/library/FavoriteCardItem.vue
@@ -1,7 +1,7 @@
 <template>
     <CardItem v-bind="itemProps">
         <template #cover>
-            <p class="status">{{ novel.status }}</p>
+            <p class="status">{{ novelStatus }}</p>
         </template>
     </CardItem>
 </template>
@@ -9,7 +9,7 @@
 <script setup>
 import CardItem from "@/components/reusable/novel/CardItem.vue";
 import Tag from "@/components/reusable/Tag.vue";
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 const props = defineProps({
     novel: {
@@ -28,16 +28,29 @@ const itemProps = {
     link: { name: "novel", params: { id: props.novel.id } },
 };
 
+const statusColor = ref("green");
+
 const novelStatus = computed(() => {
     switch (props.novel.status) {
         case "ONGOING":
+            statusColor.value = "green";
             return "연재중";
             break;
+        case "DELAYED":
+            statusColor.value = "orange";
+            return "휴재";
+            break;
         case "GIVEUP":
-            return "연중";
+            statusColor.value = "red";
+            return "연재 중단";
+            break;
+        case "COMPLETED":
+            statusColor.value = "white";
+            return "완결";
             break;
         default:
-            return "알 수 없음";
+            statusColor.value = "gray";
+            return "불명";
     }
 });
 </script>
@@ -59,5 +72,5 @@ const novelStatus = computed(() => {
 
     font-size: 0.7rem
     color: white
-    background-color: green
+    background-color: v-bind(statusColor)
 </style>
